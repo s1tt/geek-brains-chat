d39e1385f78433658303d6a627ab07d9f97b1540
Sergey Alekseev <nec13@mail.ru>
2022-05-14 15:44:41 +0300

    database added

---
+++ gb-chat-server/src/main/java/com/geekbrains/chat/server/DbAuthService.java
@@ -0,0 +1,90 @@
+package com.geekbrains.chat.server;
+
+import java.io.IOException;
+import java.sql.*;
+
+public class DbAuthService implements AuthService {
+
+
+
+    private static Connection connection;
+    private static PreparedStatement prStmt;
+    private static Statement stm;
+
+
+    public static void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:gb-chat-server/clients.db");
+            stm = connection.createStatement();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Невозможно подключиться к БД");
+        }
+    }
+
+    public static void disconnect() {
+        try {
+            if (stm != null) {
+                stm.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (prStmt != null) {
+                prStmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createFirstTable() {
+        try {
+            stm.executeUpdate("create table if not exists users (" +
+                    "id integer primary key autoincrement," +
+                    "login text not null," +
+                    "password text not null," +
+                    "nick text not null," +
+                    "unique (login, password, nick))");
+            stm.executeUpdate("insert or ignore into users (login, password, nick) values ('admin1', '123', 'Alex'), ('admin2', '123', 'Bob'), ('admin3', '123', 'Max')");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getNickByLoginAndPassword(String login, String password) {
+        try {
+            prStmt = connection.prepareStatement("select nick from users where login = ? and password = ?");
+            prStmt.setString(1, login);
+            prStmt.setString(2, password);
+            try (ResultSet rs = prStmt.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getString(1);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("AuthService run");
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("AuthService closed");
+    }
+}
